74b515fb49a29b7ea7a7d3cc7514bc4e1be69b75
Shaikh Ayaz <Sk.Ayaz2017@gmail.com>
2025-07-16 22:23:16 +0000

    update the theme switch icon to brush icon for better ux

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import * as React from 'react';
-import { Moon, Sun, Palette } from 'lucide-react';
+import { Brush } from 'lucide-react';
 import { useTheme } from '@/components/theme-provider';
 import { Button } from '@/components/ui/button';
 
@@ -10,9 +10,7 @@ export function ThemeToggle() {
 
   return (
     <Button variant="outline" size="icon" onClick={cycleTheme}>
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-       <Palette className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all group-data-[theme=light]:rotate-0 group-data-[theme=light]:scale-100" />
+      <Brush className="h-[1.2rem] w-[1.2rem]" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
