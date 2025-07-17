1ad866744b62034e880b4bca253c3fa69e4afb30
Shaikh Ayaz <Sk.Ayaz2017@gmail.com>
2025-07-17 08:13:37 +0000

    update about section and theme

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -9,7 +9,7 @@ export function HeroSection() {
       <div className="max-w-4xl">
         <div className="flex items-center gap-4">
           <Image
-            src="https://placehold.co/80x80.png"
+            src="/az-80.png"
             alt="Ayaz's avatar"
             width={80}
             height={80}
